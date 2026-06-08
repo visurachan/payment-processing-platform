741952524009b44adebb5e54576ac638d2bae04d
Visura Chandula <visurachandula@gmail.com>
2026-06-09 00:00:34 +0100

    gateway routing and security configured

--- api-gateway/src/main/java/com/payment_processing_platform/api_gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/payment_processing_platform/api_gateway/config/SecurityConfig.java
@@ -1,4 +1,22 @@
 package com.payment_processing_platform.api_gateway.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeExchange(auth -> auth
+                        .anyExchange().permitAll()
+                );
+        return http.build();
+    }
 }
